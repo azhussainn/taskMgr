0033ff34a55f1ebb632b52d1c5c216fe17973cdb
Azhar Hussain Khan <azhuss77@gmail.com>
2021-04-30 00:55:21 +0530

    make it better

--- server.js
+++ server.js
@@ -27,9 +27,15 @@ app.use("/api", routesUrl)
 
 //serve static assets
 
-app.get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>")
-  })
+
+
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 
 
